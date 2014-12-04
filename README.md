576238e10973d148b4054ad49d8d3b534a6122c3
Andrew Slotin <andrew.slotin@gmail.com>
2014-12-04 01:57:58 +0100

    git_rebase_edit_commit uses last 10 commits as a prompt list.

---
+++ README.md
@@ -0,0 +1,15 @@
+GitRebase
+=========
+
+A plugin for Sublime Text 3 that makes git rebase a little bit more convenient. 
+
+Installation
+------------
+
+Go to your Sublime Text 3 packages directory and `git clone` this repository.
+
+OS X:
+```bash
+cd "$HOME/Library/Application Support/Sublime Text 3/Packages"
+git clone git@github.com:andrewslotin/GitRebase.git
+```
--- git_rebase.py
+++ git_rebase.py
@@ -1,19 +1,41 @@
 import sublime, sublime_plugin
 import re
+import os
 
-class GitRebase(sublime_plugin.WindowCommand):
-  git_command = "git"
+from .git import Git
 
-  @staticmethod
-  def run(command):
-    sublime.status_message("{0} {1}".format(GitRebase.git_command, command))
+class GitCommand(object):
+  def _git(self):
+    cwd = self.__get_cwd()
+    if not hasattr(self, '__cached_git') or cwd != self.__cached_cwd:
+      self.__cached_cwd = cwd
+      self.__cached_git = Git(self.__cached_cwd)
 
-class GitRebaseEditCommitCommand(sublime_plugin.WindowCommand):
+    return self.__cached_git
+
+  def __get_cwd(self):
+    view = self.window.active_view()
+    if view and view.file_name() and len(view.file_name()) > 0:
+      return os.path.realpath(os.path.dirname(view.file_name()))
+
+    try:
+      return self.window.folders()[0]
+    except IndexError:
+      return None
+
+class GitRebaseEditCommitCommand(GitCommand, sublime_plugin.WindowCommand):
   def run(self):
-    self.window.show_input_panel("Commit hash:", self._selected_rev(), self.on_commit_enter, None, None)
+    commits = self._git().log(10)
+    self.window.show_quick_panel([[msg, rev] for rev, msg in commits.items()], self.on_commit_enter_handler(list(commits.keys())))
 
-  def on_commit_enter(self, rev):
-    self._git_rebase_interactive("{0}~1".format(rev))
+  def on_commit_enter_handler(self, revisions):
+    def on_commit_enter(rev):
+      if rev < 0:
+        return
+
+      sublime.status_message("rebase --interactive {0}~1".format(revisions[rev]))
+
+    return on_commit_enter
 
   def _selected_rev(self):
     rev_regex = re.compile(r'\A\s*(?P<rev>[0-9a-z]{6,40})\s*\Z')
@@ -28,19 +50,16 @@ class GitRebaseEditCommitCommand(sublime_plugin.WindowCommand):
 
     return ""
 
-  def _git_rebase_interactive(self, onto):
-    GitRebase.run("rebase --interactive {0}".format(onto))
-
-class GitRebaseAbortCommand(sublime_plugin.WindowCommand):
+class GitRebaseAbortCommand(GitCommand, sublime_plugin.WindowCommand):
   def run(self):
     self._git_rebase_abort()
 
   def _git_rebase_abort(self):
-    GitRebase.run("rebase --abort")
+    sublime.status_message("rebase --abort")
 
-class GitRebaseContinueCommand(sublime_plugin.WindowCommand):
+class GitRebaseContinueCommand(GitCommand, sublime_plugin.WindowCommand):
   def run(self):
     self._git_rebase_continue()
 
   def _git_rebase_continue(self):
-    GitRebase.run("rebase --continue")
+    sublime.status_message("rebase --continue")
